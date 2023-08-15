de499bd6ecc4f195eba556cbe89cfbe8200b9f28
Ahmed Farghal <me@asoli.dev>
2023-08-15 15:20:39 +0300

    builder pattern, and more docs

--- cling/src/command.rs
+++ cling/src/command.rs
@@ -1,6 +1,31 @@
 use crate::args::CollectedArgs;
 use crate::prelude::CliError;
 
+/// Marks clap structs as cling runnable.
+///
+/// This trait needs to be derived for clap structs or enums that will run a
+/// function handler or a if has subcommands. Note that cling requires that all
+/// clap structs/enums implement [Clone].
+///
+/// Usually, this will be derived like the following example:
+/// ```
+/// 
+/// use cling::prelude::*;
+///
+/// #[derive(CliRunnable, Parser, Debug, Clone)]
+/// #[cling(run = "do_nothing")]
+/// pub struct App {
+///     /// Turn debugging information on
+///     #[arg(short, long, action = ArgAction::Count)]
+///     pub debug: u8,
+/// }
+///
+/// fn do_nothing() {}
+/// ```
+///
+/// Runnable structs will execute the handler specified in `#[cling(run =
+/// "...")]` The string value must be a valid path to a function. See details
+/// about how to write cling handlers in [super::handler] module.
 #[async_trait::async_trait]
 pub trait CliRunnable: Send + Sync {
     async fn call(&self, args: &mut CollectedArgs) -> Result<(), CliError>;
--- cling/src/extractors.rs
+++ cling/src/extractors.rs
@@ -1,6 +1,35 @@
 use crate::args::{CliParam, CollectedArgs};
 
-// Extractor for the state
+/// Extractor for state in handlers
+///
+/// When a cling program gets executed with a state of type `S`. The state can
+/// be accessed via this extractor in any handler. There are a few restrictions
+/// on the type `S`. It must implement `Clone` and it must be `Send + Sync`.
+/// Typically, this is achieved by wrapping the state in an `Arc<Mutex<_>>`.
+///
+/// Example:
+/// ```rust
+/// use cling::prelude::*;
+///
+/// #[derive(CliParam, ValueEnum, Debug, Clone)]
+/// pub enum Colors {
+///     Red,
+///     Green,
+///     Blue,
+/// }
+///
+/// #[derive(Clone, Debug)]
+/// struct Database {
+///     _data: String,
+/// }
+///
+/// async fn init(
+///     State(database): State<Database>,
+///     colors: Option<Vec<Colors>>,
+/// ) {
+///     println!( ">> Hello world! color: {:?}, database: {:?}", colors, database);
+/// }
+/// ```
 #[derive(Debug, Clone)]
 pub struct State<S: Clone + Send + Sync + 'static>(pub S);
 
