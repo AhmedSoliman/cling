500f512170a2024ebe25f425ccbb526543a4e3ad
Ahmed Farghal <me@asoli.dev>
2023-12-01 16:23:07 +0000

    Fix release build bug at cling_handler macro

--- cling-derive/src/lib.rs
+++ cling-derive/src/lib.rs
@@ -2,11 +2,12 @@
 //! Do not depend on this library directly. Instead, use `cling`
 
 mod attributes;
+#[cfg(debug_assertions)]
 mod cling_handler;
 mod derives;
 
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput, ItemFn};
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Run, attributes(cling, command, clap))]
 pub fn derive_run(input: TokenStream) -> TokenStream {
@@ -23,10 +24,11 @@ pub fn derive_collect(input: TokenStream) -> TokenStream {
 #[proc_macro_attribute]
 pub fn cling_handler(_attr: TokenStream, function: TokenStream) -> TokenStream {
     #[cfg(not(debug_assertions))]
-    return input;
+    return function;
 
     #[cfg(debug_assertions)]
     {
+        use syn::ItemFn;
         let input = parse_macro_input!(function as ItemFn);
         cling_handler::expand_handler(&input).into()
     }
