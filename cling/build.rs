24a76f96f98e024ef46543439863c91497a58b9d
Ahmed Farghal <me@asoli.dev>
2025-05-04 12:04:24 +0100

    Upgrading dependencies and move MSRV to 1.77

--- cling/build.rs
+++ cling/build.rs
@@ -1,9 +1,10 @@
 use rustc_version::{version_meta, Channel};
 
 fn main() {
+    println!("cargo::rustc-check-cfg=cfg(unstable)");
     // Set cfg flags depending on release channel. We use "unstable" cfg to gate
     // some of the unstable features in the compiler.
     if version_meta().unwrap().channel == Channel::Nightly {
-        println!("cargo:rustc-cfg=unstable");
+        println!("cargo::rustc-cfg=unstable");
     }
 }
